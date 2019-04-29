b6536ea08a139c6133a83cd26d88daa559955f64
Cumin Lo <CuCuCumin@gmail.com>
2019-04-29 14:15:32 +0800

    添加一个简单的判断

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-wget -c https://glare.now.sh/shawn1m/overture/overture-linux-amd64 -O /tmp/overture.zip
-
-cd /tmp && unzip overture.zip && mv overture-linux-amd64 /usr/local/bin/overture
+if [ ! -f "/usr/local/bin/overture" ]; then
+  wget -c https://glare.now.sh/shawn1m/overture/overture-linux-amd64 -O /tmp/overture.zip
+  cd /tmp && unzip overture.zip && mv overture-linux-amd64 /usr/local/bin/overture
+fi
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
